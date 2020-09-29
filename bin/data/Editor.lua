289c41e974ec06c50ad4068c2588dfcfd95943c2
metalmario971 <america971@gmail.com>
2020-09-29 03:01:15 -0500

    Readme. Uncommented sound creation in main script.

--- bin/data/Editor.lua
+++ bin/data/Editor.lua
@@ -5,22 +5,12 @@ local win = Gu.getGraphicsApi():createWindow(GraphicsWindowCreateParameters("Mai
 false, true, false,
 nil
 ))
--- config.w, config.h, config.fs, true, config.ar, nil)
---local ui = Gu.getUiManager()
-
-
--- const string_t& title, int32_t x, int32_t y, int32_t width, int32_t height,
--- bool fullscreen, bool show, bool forceAspectRatio,
--- bool closeButton, bool minButton, max,  
---bool resizable, bool border,
--- std::shared_ptr<GraphicsWindow> parent = nullptr
-
-
 local win_child = Gu.getGraphicsApi():createWindow(GraphicsWindowCreateParameters("Child Window",
 100, 100, 200, 500,  GraphicsWindowCreateParameters.Wintype_Utility,
 false, true, false,
 win
 ))
 local file = "nature_0.ogg"
---Gu.playSound(file, SoundPlayInfo(true, 0, 1))
+--This is causing "bad arg" errors although it is correct.
+local sound = Gu.playSound(file, SoundPlayInfo(true, 0, 1))
 print("Lua: Done")
